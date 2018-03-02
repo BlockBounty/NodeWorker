e9d3d42fcf6ff62a44ec6534af86dd9b487e7191
devin.kiser <devin.kiser@asynchrony.com>
2018-03-01 19:43:05 -0600

    CLI options to get required input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('ayyyy');
+let argv = require('yargs')
+    .usage('Usage: -a [ether address] -j [job] -s [api_server]')
+    .describe('a', 'Ethereum address to pay shares of the bounty to')
+    .describe('j', 'Job id to contribute to')
+    .describe('s', 'Optional parameter to specify the api server to use')
+    .alias('a', 'address')
+    .alias('j', 'job')
+    .alias('s', 'server')
+    .demandOption(['a', 'j'])
+    .help('h')
+    .alias('h', 'help')
+    .default({ 's': 'http://localhost:8089' })
+    .number('j')
+    .string(['a', 's'])
+    .argv;
+
+console.log(JSON.stringify(argv));
